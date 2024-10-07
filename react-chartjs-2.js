5efa71ac5a788e77febe32857f11a21e6be3e2d9
jrsmtt <163967198+jrsmtt@users.noreply.github.com>
2024-10-07 20:10:49 +0530

    Create react-chartjs-2.js

---
+++ react-chartjs-2.js
@@ -0,0 +1,16 @@
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }
+  ]
+};
+
+<Line data={data} />;
